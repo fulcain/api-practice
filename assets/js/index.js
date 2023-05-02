9849dd796a8457bfe721702d2871864481c43a7f
dofxo <kargat504@gmail.com>
2023-05-02 16:56:06 +0330

    feat(library): add silverBox library

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,6 @@
-function jasem() {
-    const xhr = new XMLHttpRequest()
+import silverBox from "../libraries/silverBox_1.0.0-rc6_min/silverBox.min.js";
 
-    xhr.open("GET", "jasmem", true)
-
-    xhr.onload = function(){
-
-    }
-    xhr.send()
-}
+silverBox({
+    title: 'YAMETE',
+    titleCustomIcon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_yiWd9F-_s4hjqq3Ql8dIoIR0Qu-_mSryqg&usqp=CAU"
+})
