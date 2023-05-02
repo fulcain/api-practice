298fb82c244c6d1f629ef3486e0ad1452b1bddad
Majid Kargar <majidkargar01@gmail.com>
2023-05-02 17:20:39 +0430

    feat(index.js): add inputTemplate

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,36 @@
 import silverBox from "../libraries/silverBox_1.0.0-rc6_min/silverBox.min.js";
 
-const oiOiOi = `
-<!-- input section -->
-<section id="name-fake-input">
-    <div class="container">
-        <!-- input wrapper -->
-        <div class="input-wrapper">
-            <!-- label -->
-            <label for="gender">Gender</label>
-            <!-- select -->
-            <select name="gender" id="gender">
-                <option value="random">random</option>
-                <option value="male">male</option>
-                <option value="female">female</option>
-            </select>
-        </div>
-    </div>
-</section>`
+function inputTemplate() {
+    return (
+        `
+        <!-- input section -->
+        <section id="name-fake-input">
+            <div class="container">
+                <!-- input wrapper -->
+                <div class="input-wrapper">
+                    <!-- label -->
+                    <label for="gender">Gender</label>
+                    <!-- select -->
+                    <select name="gender" id="gender">
+                        <option value="random">random</option>
+                        <option value="male">male</option>
+                        <option value="female">female</option>
+                    </select>
+                </div>
+            </div>
+        </section>`
+    )
+}
+
 const startApp = document.querySelector('#startApp')
-startApp.addEventListener("click",() =>{
+startApp.addEventListener("click", () => {
     silverBox({
-    silverBoxId:"nameFakeInput",
-    theme: "dark",
-    html: oiOiOi,
-    confirmButton:{
-        text:"generate",
-        closeOnClick:true
-    },
-})
+        silverBoxId: "nameFakeInput",
+        theme: "dark",
+        html: inputTemplate(),
+        confirmButton: {
+            text: "generate",
+            closeOnClick: true
+        },
+    })
 })
