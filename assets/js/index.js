29fa162447c245cdb735c7d3d69b66246eb425b2
dofxo <kargat504@gmail.com>
2023-05-02 19:01:09 +0330

    feat(result modal): add a result modal after generateBtn click

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,50 @@
 import silverBox from "../libraries/silverBox_1.0.0-rc6_min/silverBox.min.js";
 
+const startApp = document.querySelector('#startApp')
+
+startApp.addEventListener("click", () => {
+    // creating a popUp modal
+    silverBox({
+        silverBoxId: "nameFakeInput",
+        theme: "dark",
+        html: inputTemplate(),
+        confirmButton: {
+            buttonId: 'generate-button',
+            text: "Generate",
+            closeOnClick: false
+        },
+    })
+
+    // calling generate function
+    generate()
+
+
+})
+
+// generate button event listner
+function generate() {
+    // select generate button
+    let generateBtn = document.querySelector("#generate-button")
+
+    generateBtn.addEventListener('click', () => {
+        silverBox({
+            title: "Result",
+            theme: 'dark',
+            removePrevLoadings: 'all',
+            removePrevBoxes: 'all',
+            customIconId: 'icon',
+            customIcon: "assets/images/male.svg",
+            html: resultTemplate(),
+            centerContent: true,
+        })
+
+    })
+}
+
+
+
+
+// templates
 function inputTemplate() {
     return (
         `
@@ -21,16 +66,15 @@ function inputTemplate() {
         </section>`
     )
 }
-
-const startApp = document.querySelector('#startApp')
-startApp.addEventListener("click", () => {
-    silverBox({
-        silverBoxId: "nameFakeInput",
-        theme: "dark",
-        html: inputTemplate(),
-        confirmButton: {
-            text: "generate",
-            closeOnClick: true
-        },
-    })
-})
+function resultTemplate() {
+    return (` <section id="result">
+        <div id="information">
+            <div>Name: </div>
+            <div>Gender: </div>
+            <div>Email: </div>
+            <div>Height: </div>
+            <div>Weight: </div>
+            <div>Phone: </div>
+        </div>
+    </section>`)
+}
